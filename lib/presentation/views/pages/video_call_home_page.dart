d8c45393a230dc4f3c82628140100156bdb7f6d8
Amr Ali <140944803+Amr-3li@users.noreply.github.com>
2025-09-02 09:32:21 +0300

    make join meeting room in home page

--- lib/presentation/views/pages/video_call_home_page.dart
+++ lib/presentation/views/pages/video_call_home_page.dart
@@ -1,18 +1,17 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:vedio_call/core/services/font_size_handeler.dart';
 import 'package:vedio_call/presentation/views/widgets/background_app.dart';
 import 'package:vedio_call/presentation/views/widgets/row_appbaer.dart';
 
 import '../widgets/create_room_container.dart';
+import '../widgets/join_room_container.dart';
 
 class VideoCallHomeScreen extends StatelessWidget {
   const VideoCallHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: BackgroundApp(
         child: Column(
@@ -27,33 +26,3 @@ class VideoCallHomeScreen extends StatelessWidget {
   }
 }
 
-class JoinRoomContainer extends StatelessWidget {
-  const JoinRoomContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Join Room",
-          style: TextStyle(
-            fontSize: getResposiveSize(context, fontSize: 30),
-            fontWeight: FontWeight.w900,
-          ),
-        ),
-        const SizedBox(height: 20),
-        TextFormField(
-          onTapUpOutside:  (event) =>  FocusManager.instance.primaryFocus?.unfocus(),
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: BorderSide(color: Colors.black, width: 1),
-            ),
-            enabledBorder:  OutlineInputBorder()
-          ),
-        ),
-      ],
-    );
-  }
-}
---
+++ lib/presentation/views/widgets/join_room_container.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:vedio_call/core/constants/colors.dart';
+import 'package:vedio_call/presentation/views/widgets/button_widget.dart';
+
+import '../../../core/services/font_size_handeler.dart';
+
+class JoinRoomContainer extends StatelessWidget {
+  const JoinRoomContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController roomIdController = TextEditingController();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Join Room",
+          style: TextStyle(
+            fontSize: getResposiveSize(context, fontSize: 30),
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Container(
+          decoration: BoxDecoration(
+            color: MyColors.iconColor,
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: Colors.grey.shade300),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.02),
+                blurRadius: 8,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+
+          child: TextField(
+            controller: roomIdController,
+            decoration: const InputDecoration(
+              hintText: 'Enter room ID',
+              hintStyle: TextStyle(color: Colors.grey, fontSize: 16),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 16,
+              ),
+            ),
+            style: const TextStyle(fontSize: 16, color: Colors.black),
+          ),
+        ),
+        const SizedBox(height: 20),
+        ButtonWidget(text: "Join", onPressed: () {}),
+      ],
+    );
+  }
+}
